05513c62b59d57cda8f6d1344e0c161b3d82eb73
dugasmate <dugasmate@gmail.com>
2018-07-03 11:53:51 +0200

    sighting model and repository layer added

---
+++ Seen/Seen/Repository/SightingRepository.cs
@@ -0,0 +1,51 @@
+﻿using Seen.Entities;
+using Seen.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Seen.Repository
+{
+    public class SightingRepository
+    {
+
+        private SeenContext seenContext;
+
+        public SightingRepository(SeenContext seenContext)
+        {
+            this.seenContext = seenContext;
+        }
+
+        public async Task CreateAsync(Sighting sighting)
+        {
+            await seenContext.Sightings.AddAsync(sighting);
+            await seenContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(long id)
+        {
+            var sightingToDelete = await SelectByIdAsync(id);
+            seenContext.Remove(sightingToDelete);
+            await seenContext.SaveChangesAsync();
+        }
+
+        public async Task<List<Sighting>> SelectAllAsync()
+        {
+            var sightingList = await feedbackContext.Questions.ToListAsync();
+            return sightingList;
+        }
+
+        public async Task<Question> SelectByIdAsync(long id)
+        {
+            var questionToFind = await feedbackContext.Questions.FindAsync(id);
+            return questionToFind;
+        }
+
+        public async Task UpdateAsync(Question question)
+        {
+            feedbackContext.Questions.Update(question);
+            await feedbackContext.SaveChangesAsync();
+        }
+    }
+}
