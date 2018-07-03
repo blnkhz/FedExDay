9277cbcb20f0d54d162bea859c3da3a402d9121a
dugasmate <dugasmate@gmail.com>
2018-07-03 10:59:00 +0200

    removed stuff

--- Seen/Seen/Controllers/HomeController.cs
+++ Seen/Seen/Controllers/HomeController.cs
@@ -4,7 +4,6 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Seen.Models;
 
 namespace Seen.Controllers
 {
@@ -13,25 +12,6 @@ namespace Seen.Controllers
         public IActionResult Index()
         {
             return View();
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
